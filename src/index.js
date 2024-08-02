e1ab75db29a00c2d86eac7404dec332147b0da93
Nathan <Nathan.rhoden@gmail.com>
2024-08-02 19:39:06 +0100

    Added Append and remove letter functions

--- src/index.js
+++ src/index.js
@@ -1,11 +1,35 @@
 const alphabetCheckRegex = /^[a-zA-Z]+$/;
 
-window.addEventListener('keydown', (event) => {
-    const pressedKey = event.key;
-   
-    console.log(typeof pressedKey);
-    
-    if (alphabetCheckRegex.test(pressedKey)) {
-        document.getElementById('1').innerHTML = pressedKey.toUpperCase();
+let currentWord = "";
+let currentSquare = 0;
+let letterCount = 0; 
+
+window.addEventListener("keydown", (event) => {
+  const pressedKey = event.key;
+
+ 
+    if (alphabetCheckRegex.test(pressedKey) && pressedKey.length === 1) {
+      document.getElementById(currentSquare.toString()).innerHTML = pressedKey.toUpperCase();
+  
+      appendLetterToWord(pressedKey.toUpperCase());
+  
+    } else if (pressedKey == "Backspace" && currentSquare > 0 ) {
+      removeLetterFromWord();
     }
+
+  
 });
+
+function appendLetterToWord(letter) {
+  currentWord = currentWord.concat(letter);
+  currentSquare++;  
+  console.log(currentWord);
+}
+
+function removeLetterFromWord() {
+  currentSquare--;
+  document.getElementById(currentSquare.toString()).innerHTML = " ";
+  currentWord = currentWord.substring(0, currentWord.length - 1);
+  console.log(currentWord);
+}
+
